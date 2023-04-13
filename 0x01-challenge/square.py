4c797dc2e456ae3f59c104b3a0b6941cbe567686
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-04-13 16:13:48 +0300

    more edits made to the class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,59 @@
 #!/usr/bin/python3
 """Module contains the Square class"""
 
+
 class Square():
     """`Square` class showing the `area` and `perimeter` functions"""
 
-    width = 0
-    height = 0
+    __width = 0
+    __height = 0
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, width=0, height=0):
         """Initialize the variables"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter for the private instance attribute height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width ** 2
+        return (self.__width * self.__height)
 
     def perimeterOfMySquare(self):
         """ Perimeter of the square"""
-        return (4 * self.width)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """String representation of the Square"""
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.__width, self.__height)
 
 
 if __name__ == "__main__":
